04f44758b7d78de5f528da0693c2c644ff676fe3
basusisa <siddarthjawahar@gmail.com>
2016-04-26 00:39:38 +0530

    Update Python input GUI with csefaculty and crossoversplit

--- gui_vars.py
+++ gui_vars.py
@@ -1,13 +1,13 @@
 from Tkinter import *
 import csv
 
-def csv_maker(periods,teachers,rooms,Tsize,rate,Psize,limit,elite):
+def csv_maker(periods,teachers,rooms,Tsize,rate,Psize,limit,elite,C_Split,C_Faculty):
     filename = 'csv/variables.csv'
     with open(filename,'wb') as csvfile:
-        fields = ['No_of_periods_per_week','No_of_teachers','No_of_rooms','Tournament_size','Mutation_rate','Population_size','Generation_limit', 'Elitism']
+        fields = ['No_of_periods_per_week','No_of_teachers','No_of_rooms','Tournament_size','Mutation_rate','Population_size','Generation_limit', 'Elitism','Crossover_split','CSE_faculty_count']
         file_writer = csv.DictWriter(csvfile,fieldnames=fields)
         file_writer.writeheader()
-        file_writer.writerow({'No_of_periods_per_week':periods,'No_of_teachers':teachers,'No_of_rooms':rooms,'Tournament_size':Tsize,'Mutation_rate':rate,'Population_size':Psize,'Generation_limit':limit,'Elitism':elite})
+        file_writer.writerow({'No_of_periods_per_week':periods,'No_of_teachers':teachers,'No_of_rooms':rooms,'Tournament_size':Tsize,'Mutation_rate':rate,'Population_size':Psize,'Generation_limit':limit,'Elitism':elite,'Crossover_split':C_Split,'CSE_faculty_count':C_Faculty})
         master.destroy()
     #c = csv.writer(open("test.csv","wb"))
 
@@ -65,7 +65,26 @@ E8 = Entry(master, bd=3)
 E8.insert(0,1)
 E8.place(x=300,y=160)
 
-submit=Button(master, text = "Submit", command = lambda: csv_maker(E1.get(),E2.get(),E3.get(),E4.get(),E5.get(),E6.get(),E7.get(),E8.get()))
+L9 = Label(master, text = "Crossover Split")
+L9.place(x=540,y=140)
+E9 = Entry(master, bd=3)
+E9.insert(0,16)
+E9.place(x=540,y=160)
+
+Ltemp1 = Label(master, text ="/", font = ("Times",30))
+Ltemp1.place(x=670,y=150)
+
+Ltemp2 = Label(master, text ="no. of peroids per week", font = ("Times",13))
+Ltemp2.place(x=684,y=163)
+
+L10 = Label(master, text = "CSE Faculty Count")
+L10.place(x=60,y=210)
+E10 = Entry(master, bd=3)
+E10.insert(0,43)
+E10.place(x=60,y=230)
+
+
+submit=Button(master, text = "Submit", command = lambda: csv_maker(E1.get(),E2.get(),E3.get(),E4.get(),E5.get(),E6.get(),E7.get(),E8.get(),E9.get(),E10.get()))
 submit.place(x=600, y=650)
 #submit.pack()
 
