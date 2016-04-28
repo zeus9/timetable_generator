3b596378e3e999847e7ba43d646e307c92101163
zeus9 <sambhav1994jain@gmail.com>
2016-04-28 09:15:27 +0530

    Add room checking in labConflicts.csv

--- labConflictsGen.py
+++ labConflictsGen.py
@@ -1,6 +1,7 @@
 import csv
 
 idlist = []
+roomlist = []
 
 def csv_reader(filename = 'csv/labCsv/labPeriodcount.csv'):
     with open(filename,'r') as csvfile:
@@ -8,16 +9,17 @@ def csv_reader(filename = 'csv/labCsv/labPeriodcount.csv'):
         for row in reader:
             tid = row['id'].split('/',1)
             idlist.append(tid[0])
+            roomlist.append(tid[1])
             
 def csv_maker(filename = 'csv/labCsv/labConflicts.csv'):
     with open(filename,'wb') as csvfile:
 #        writer = csv.DictWriter(csvfile,fieldnames = idlist)
         writer = csv.writer(csvfile, delimiter = ',')
 #        writer.writeheader()
-        for tid1 in idlist:
+        for tid1,room1 in zip(idlist,roomlist):
             rowline = []
-            for tid2 in idlist:
-                if tid1 == tid2:
+            for tid2,room2 in zip(idlist,roomlist):
+                if tid1 == tid2 or room1 == room2:
                     rowline.append('1')
                 else:
                     rowline.append('0')
